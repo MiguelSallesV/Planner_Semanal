612726516e7e9ee1662b2b44061da507041f3566
Miguel Salles <miguelsallesdovale@gmail.com>
2025-05-05 11:47:28 -0300

    Saving and Deleting Local Data with LocalStorage.
    I used a web API to save local data in the browser. The files remain in the browser even if you close or refresh it.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+
 function criarEvento(event) {
     let botaoClicado = event.target;
-    let dia = botaoClicado.value
+    let dia = botaoClicado.value;
     document.getElementById('formulario').style.display = 'block';
     document.getElementById('fundo').style.display = 'block';
-    document.getElementById('diaselecionado').value = dia
+    document.getElementById('diaselecionado').value = dia;
 }
 
 function fecharFormulario() {
@@ -11,6 +13,7 @@ function fecharFormulario() {
     document.getElementById('fundo').style.display = 'none';
 }
 
+//Pegando as datas atuais
 let data = new Date();
 let anoAtual = data.getFullYear();
 let mesAtual = data.getMonth();
@@ -19,26 +22,67 @@ let horaAtual = data.getHours();
 let mensagemBomDia = document.querySelector('h2.diaatual__header');
 
 if (horaAtual <= 12 && horaAtual > 5) {
-    mensagemBomDia.innerHTML = `Bom dia! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`    
+    mensagemBomDia.innerHTML = `Bom dia! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`;    
 } else if (horaAtual > 12 && horaAtual <= 17) {
-    mensagemBomDia.innerHTML = `Boa Tarde! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`   
-} else if (horaAtual > 0 && horaAtual <= 5) {
-    mensagemBomDia.innerHTML = `Boa Madrugada! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`
+    mensagemBomDia.innerHTML = `Boa Tarde! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`; 
+} else if (horaAtual >= 0 && horaAtual <= 5) {
+    mensagemBomDia.innerHTML = `Boa Madrugada! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`;
 } else {
-    mensagemBomDia.innerHTML = `Boa Noite! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`
+    mensagemBomDia.innerHTML = `Boa Noite! Hoje é ${diaAtual}/${mesAtual + 1}/${anoAtual}`;
+}
+
+//Função que adiciona os eventos a lista do dia específico
+function exibirEventos(dia) {
+    let listaDia = document.querySelector(`ul.lista__${dia}`);
+    listaDia.innerHTML = '';
+    let eventos = JSON.parse(localStorage.getItem(dia)) || [];
+    eventos.forEach((evento) => {
+        var elemento = document.createElement('li');
+        elemento.style.borderTop = '5px #6F826A solid';
+        elemento.innerHTML = evento.descricao
+        ? `${evento.titulo}<br>${evento.descricao}<br><span class="horario">${evento.horario}</span>`
+        : `${evento.titulo}<br><span class="horario">${evento.horario}</span>`;
+        listaDia.appendChild(elemento);
+    })
 }
 
 function adicionarEvento(event) {
-    event.preventDefault()
+    event.preventDefault() //Evita o carregamento da página ao enviar o formulário
+
     let diaSelecionado = document.getElementById('diaselecionado').value;
-    document.querySelector('ul.lista__dia').style.display = 'block'
+    document.querySelector('ul.lista__dia').style.display = 'block';
     let formulario = document.querySelector('form.formulario__campos');
-    let titulo = document.getElementsByName('titulo')[0].value
-    let descricao = document.getElementsByName('descricao')[0].value
-    let horario = document.getElementsByName('horario')[0].value
-    var evento = document.createElement('li')
-    if (diaSelecionado === 'segunda') {
-        evento.innerHTML = `${titulo}, ${descricao}, ${horario}`
-        document.querySelector('ul.lista__segunda').appendChild(evento)       
-    }
+    let titulo = document.getElementsByName('titulo')[0].value;
+    let descricao = document.getElementsByName('descricao')[0].value;
+    let horario = document.getElementsByName('horario')[0].value;
+
+    const eventoLocal = {
+        titulo: titulo,
+        descricao: descricao,
+        horario: horario,
+    };
+
+    //Recupera os eventos que já estão armazenados no Storage
+    let evento = JSON.parse(localStorage.getItem(diaSelecionado)) || [];
+    evento.push(eventoLocal)
+
+    //Salva os eventos atualizados no LocalStorage
+    localStorage.setItem(diaSelecionado, JSON.stringify(evento));
+    
+    exibirEventos(diaSelecionado);
+
+    formulario.submit()
+}
+//Carregar eventos automaticamente quando atualizar a página
+    document.addEventListener('DOMContentLoaded', () => {
+        const diasDaSemana = ['segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo'];
+
+        diasDaSemana.forEach(dia => {
+            exibirEventos(dia);
+        });
+    });
+
+function excluirAllEvents() {
+    localStorage.clear()
+    location.reload()
 }
